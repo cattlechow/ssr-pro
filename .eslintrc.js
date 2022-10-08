860394b11d6b37d4aff155072654c84b2271a8e1
Cattle <cattle@192.168.0.100>
2022-10-08 16:47:22 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true, // ADD, 支持对commonjs全局变量的识别
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-var-requires": "off",
+  },
+};
---
+++ index.jsx
@@ -0,0 +1,5 @@
+const a = () => {
+    return {
+        <div>
+    }
+}
